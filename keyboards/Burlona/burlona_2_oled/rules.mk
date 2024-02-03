b02f2712c24010b39bb49c2887810715d208c0b9
Burlona-Admin <christopher.george@burlona.com>
2024-02-03 10:53:29 -0500

    Add files via upload

---
+++ keyboards/Burlona/burlona_2_oled/rules.mk
@@ -0,0 +1,16 @@
+# This file intentionally left blank
+# Bootloader selection
+BOOTLOADER = rp2040
+LTO_ENABLE = yes
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
+MOUSEKEY_ENABLE = no
+EXTRAKEY_ENABLE = no
+SPACE_CADET_ENABLE = no
+GRAVE_ESC_ENABLE = no
+MAGIC_ENABLE = no
+AVR_USE_MINIMAL_PRINTF = yes
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+OLED_ENABLE = yes
+OLED_DRIVER = ssd1306
+OLED_TRANSPORT = i2c
